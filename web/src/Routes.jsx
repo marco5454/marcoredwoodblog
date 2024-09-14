898ba03e767f4edbd6b927228b0a851d316343a2
marco <marco.me.personal@gmail.com>
2024-09-14 08:38:08 +0800

    boiler code

---
+++ web/src/Routes.jsx
@@ -0,0 +1,19 @@
+import { Router, Route, Set } from '@redwoodjs/router'
+
+import BlogLayout from './layouts/BlogLayout/BlogLayout'
+
+const Routes = () => {
+  return (
+    <Router>
+      <Set wrap={BlogLayout}>
+        <Route path="/contact" page={ContactPage} name="contact" />
+        <Route path="/about" page={AboutPage} name="about" />
+        <Route path="/" page={HomePage} name="home" />
+      </Set>
+
+      <Route notfound page={NotFoundPage} />
+    </Router>
+  )
+}
+
+export default Routes
---
+++ web/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import { Metadata } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <Metadata title="Home" description="Home page" />
+
+      <h1>HomePage</h1>
+    </>
+  )
+}
+
+export default HomePage
